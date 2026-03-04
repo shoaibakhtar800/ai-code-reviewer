542aeca37e2866da5b529b3dfb28de513f2b5c3e
shoaibakhtar800 <cs08018akp@gmail.com>
2026-03-04 12:04:59 +0530

    feat: implement initial tRPC setup including API routes, client provider integration, and a basic client-side greeting component.

--- src/app/client-greeting.tsx
+++ src/app/client-greeting.tsx
@@ -1,11 +1,12 @@
 "use client";
+
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 export function ClientGreeting() {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(
-    trpc.helloRouter.hello.queryFilter({ text: "Hello" }),
+    trpc.helloRouter.hello.queryOptions({ text: "Hello" }),
   );
   return <div>{data.greeting}</div>;
 }
