4be0b2a206a5c0ef84d274ed5a3e2a4c72a8b1d7
shoaibakhtar800 <cs08018akp@gmail.com>
2026-03-04 13:00:36 +0530

    feat: Initialize and configure `better-auth` for client and server-side authentication.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -15,4 +15,15 @@ export const auth = betterAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
   },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7, // 7 days
+    updateAge: 60 * 60 * 24, // 1 day
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 5, // 5 mins
+    },
+  },
+  trustedOrigins: [process.env.NEXT_PUBLIC_BETTER_AUTH_URL!],
 });
+
+export type Session = typeof auth.$Infer.Session;
